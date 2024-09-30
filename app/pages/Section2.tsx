f96c312b54ce8435aef4e14c8f3254cbd3705b56
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-09-29 23:20:02 -0300

    earth

--- app/pages/Section2.tsx
+++ app/pages/Section2.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Projects from "@/components/shared/projects";
+import dynamic from "next/dynamic";
+import styles from "../../styles/section2.module.scss";
+
+const Earth = dynamic(() => import("@/components/shared/earth"), {
+  ssr: false,
+  loading: () => <img src="/assets/earth/placeholder.png"></img>,
+});
 
 const Section2 = () => {
-  return <div>Mapa</div>;
+  return (
+    <div>
+      <main className={styles.main}>
+        <Earth />
+        <Projects />
+      </main>
+    </div>
+  );
 };
 
 export default Section2;
