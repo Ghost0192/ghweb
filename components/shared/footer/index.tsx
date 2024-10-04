6305d176f966732e2c8053f9dc6dee825c1a3b00
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-10-04 10:21:59 -0300

    overflow

--- components/shared/footer/index.tsx
+++ components/shared/footer/index.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
       className="relative h-[600px]"
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
-      <div className="fixed bottom-0 h-[600px] w-full">
+      <div className="fixed bottom-0 h-[600px] w-full overflow-hidden">
         <Content />
       </div>
     </div>
