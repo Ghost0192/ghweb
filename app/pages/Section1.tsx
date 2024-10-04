e1c7c00be2f0236a8097f91a8ad0e39532a22318
Gaspar Goycoolea <ggoycoo@gmail.com>
2024-10-04 14:59:25 -0300

    f2

--- app/pages/Section1.tsx
+++ app/pages/Section1.tsx
@@ -1,42 +1,48 @@
 "use client";
-import { useRef, useEffect } from "react";
+
+import { useRef, useEffect, useCallback, useState } from "react";
 import styles from "../../styles/section1.module.css";
 
 export default function Section1() {
   const container = useRef<HTMLDivElement | null>(null);
   const stickyMask = useRef<HTMLDivElement | null>(null);
-
-  const initialMaskSize = 0.8;
-  const targetMaskSize = 30;
+  const initialMaskSize = useRef(0.8);
+  const targetMaskSize = useRef(30);
   const easing = 0.15;
-  let easedScrollProgress = 0;
 
-  useEffect(() => {
-    requestAnimationFrame(animate);
-  }, []);
+  const [easedScrollProgress, setEasedScrollProgress] = useState(0);
 
-  const animate = () => {
-    if (stickyMask.current) {
-      const maskSizeProgress = targetMaskSize * getScrollProgress();
-      stickyMask.current.style.maskSize =
-        (initialMaskSize + maskSizeProgress) * 100 + "%";
-      stickyMask.current.style.maskSize =
-        (initialMaskSize + maskSizeProgress) * 100 + "%";
-    }
-    requestAnimationFrame(animate);
-  };
-
-  const getScrollProgress = () => {
+  const getScrollProgress = useCallback(() => {
     if (stickyMask.current && container.current) {
       const scrollProgress =
         stickyMask.current.offsetTop /
         (container.current.getBoundingClientRect().height - window.innerHeight);
       const delta = scrollProgress - easedScrollProgress;
-      easedScrollProgress += delta * easing;
-      return easedScrollProgress;
+      return easedScrollProgress + delta * easing;
     }
     return 0;
-  };
+  }, [easedScrollProgress]);
+
+  useEffect(() => {
+    let animationFrameId: number;
+
+    const animate = () => {
+      if (stickyMask.current) {
+        const maskSizeProgress = targetMaskSize.current * getScrollProgress();
+        const newMaskSize = (initialMaskSize.current + maskSizeProgress) * 100;
+        stickyMask.current.style.maskSize = `${newMaskSize}%`;
+        stickyMask.current.style.maskSize = `${newMaskSize}%`;
+      }
+      setEasedScrollProgress(getScrollProgress());
+      animationFrameId = requestAnimationFrame(animate);
+    };
+
+    animate();
+
+    return () => {
+      cancelAnimationFrame(animationFrameId);
+    };
+  }, [getScrollProgress]);
 
   return (
     <main className={styles.main}>
